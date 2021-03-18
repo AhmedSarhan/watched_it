18b45e14439fe9b46c7513e0c5759b4ac2db7deb
Ahmed Sarhan <ahmedsarhan1515@gmail.com>
2021-03-18 02:41:28 +0200

    fix the db vars

--- api/utils/db.js
+++ api/utils/db.js
@@ -6,14 +6,9 @@ const { Sequelize } = require('sequelize');
 //   host: 'localhost',
 // });
 // hosted db
-const sequelize = new Sequelize(
-  process.env.db_name,
-  process.env.db_user,
-  process.env.db_pass,
-  {
-    dialect: 'mysql',
-    host: process.env.db_server,
-  }
-);
+const sequelize = new Sequelize('sql11398640', 'sql11398640', 'a2r9Jju7fU', {
+  dialect: 'mysql',
+  host: 'sql11.freemysqlhosting.net',
+});
 
 module.exports = sequelize;
